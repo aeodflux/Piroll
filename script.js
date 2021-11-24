8264adad42f6f82e418aff0421d2a044afd3e146
silkesssji <kastrdast@mail.ru>
2021-11-24 21:44:45 +0200

    Updates

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-var video = document.querySelector(".process__video");
-var videoButton = document.querySelector(".process__button");
 
-var isVideoOpened = true;
-
-video.addEventListener("click", function(evt) { 
-  // TODO: fix script
-  // evt.preventDefault();
-  if (isVideoOpened) {
-    videoButton.classList.remove("process__button_enabled")
-    videoButton.classList.add("process__button_disabled")
-  } 
-  else {
-    videoButton.classList.remove("process__button_disabled")
-    videoButton.classList.add("process__button_enabled")
-  }
-  isVideoOpened = !isVideoOpened
-});
   
