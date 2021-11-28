193592fb27ef85280fb41cc4a2fb45c79e2e8c55
silkesssji <kastrdast@mail.ru>
2021-11-28 23:52:59 +0200

    Add carousel library

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,32 @@
 
+const options = {
+    // [...]
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false
+        }
+      },
+    ]
+  }
+
   
