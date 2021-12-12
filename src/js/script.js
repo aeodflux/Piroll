706c6665522f0a5e805356147af59afafbf14292
silkesssji <kastrdast@mail.ru>
2021-12-12 21:07:58 +0200

    Add mobile nav

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,28 @@
 var quoteCarouselElement=document.querySelector('.slogan__carousel')
 var projectCarouseElement=document.querySelector('.project__carousel')
 
+var toggleMobileMenuButton = document.querySelector(".nav__toggle-menu");
+var menu = document.querySelector(".nav__menu");
+
+var isMenuOpened = false;
+var closingTimeout;
+toggleMobileMenuButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (!isMenuOpened) {
+    clearTimeout(closingTimeout)
+    menu.classList.add("nav__menu--opened")
+  } else {
+    // closingTimeout = setTimeout(() => 
+    menu.classList.remove("nav__menu--opened")
+    // , 800)
+  }
+  isMenuOpened = !isMenuOpened
+});
+
+
+
+
+
 if (quoteCarouselElement) {
   var slider = tns({
     container: '.slogan__carousel',
