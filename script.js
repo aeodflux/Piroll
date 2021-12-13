cdb5abdd0a829e66d1bf2351d196227629e6e80d
silkesssji <kastrdast@mail.ru>
2021-12-13 18:35:14 +0200

    Updates

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ var tns=function(){var t=window,Ai=t.requestAnimationFrame||t.webkitRequestAnima
 var quoteCarouselElement=document.querySelector('.slogan__carousel')
 var projectCarouseElement=document.querySelector('.project__carousel')
 
+var toggleMobileMenuButton = document.querySelector(".nav__toggle-menu");
+var menu = document.querySelector(".nav__menu");
+
+var isMenuOpened = false;
+toggleMobileMenuButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (!isMenuOpened) {
+    menu.classList.add("nav__menu--opened")
+    toggleMobileMenuButton.classList.add("nav__toggle-menu--close")
+  } else {
+    toggleMobileMenuButton.classList.remove("nav__toggle-menu--close")
+    menu.classList.remove("nav__menu--opened")
+  }
+  isMenuOpened = !isMenuOpened
+});
+
+
 if (quoteCarouselElement) {
   var slider = tns({
     container: '.slogan__carousel',
